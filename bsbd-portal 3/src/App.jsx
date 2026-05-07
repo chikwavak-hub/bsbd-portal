13e54cd948646b7af971646d48f05ee8a82ff799
chikwavak-hub <chikwavak@gmail.com>
2026-05-07 13:05:42 -0400

    Update App.jsx

--- bsbd-portal 3/src/App.jsx
+++ bsbd-portal 3/src/App.jsx
@@ -17,8 +17,8 @@ import AdminPage     from './pages/admin/Admin'
 import TcPatientsPage  from './pages/tc/Patients'
 import TcAlertsPage    from './pages/tc/Alerts'
 import TcDashboardPage        from './pages/tc/Dashboard'
-import PredeterminationsPage  from './pages/tc/Predeterminations'
-import RidgeviewPortal        from './pages/ridgeview/RidgeviewPortal'
+const PredeterminationsPage = React.lazy(() => import('./pages/tc/Predeterminations'))
+const RidgeviewPortal = React.lazy(() => import('./pages/ridgeview/RidgeviewPortal'))
 import CollectionTrackerPage from './pages/collections/CollectionTracker'
 import OMReviewPage       from './pages/collections/OMReview'
 import CollectionsHome    from './pages/collections/CollectionsHome'
@@ -203,7 +203,7 @@ export default function App() {
   }
 
   if (!user) return <LoginPage doLogin={doLogin} />
-  if (isRidgeview) return <RidgeviewPortal user={user} notify={notify}/>
+  if (isRidgeview) return <React.Suspense fallback={<div style={{padding:40,textAlign:'center'}}>Loading…</div>}><RidgeviewPortal user={user} notify={notify}/></React.Suspense>
 
   const isAdmin      = user.role === 'admin'
   const isRidgeview  = user.role === 'ridgeview'
@@ -237,7 +237,7 @@ export default function App() {
             {module==='collections' && collPage==='collection_tracker' && <CollectionTrackerPage user={user} isManager={isManager}/>}
             {/* TC module */}
             {module === 'tc' && page === 'tc_patients'  && isTC      && <TcPatientsPage user={user} tcPatients={tcPatients} isManager={isManager} users={users} saveTcPatient={saveTcPatient} loadTcPatients={loadTcPatients} deleteTcPatient={deleteTcPatient} notify={notify} />}
-            {module === 'tc' && page === 'tc_predeterminations' && isTC && <PredeterminationsPage user={user} isManager={isManager} tcPatients={tcPatients} users={users}/>}
+            {module === 'tc' && page === 'tc_predeterminations' && isTC && <React.Suspense fallback={null}><PredeterminationsPage user={user} isManager={isManager} tcPatients={tcPatients} users={users}/></React.Suspense>}
             {module === 'tc' && page === 'tc_alerts'    && isTC      && <TcAlertsPage tcPatients={tcPatients} collectionPatients={collectionPatients} user={user} users={users} isManager={isManager} setPage={setPage} notify={notify} saveTcPatient={saveTcPatient} />}
             {module === 'tc' && page === 'tc_dashboard' && isManager && <TcDashboardPage tcPatients={tcPatients} users={users} />}
           </div>
