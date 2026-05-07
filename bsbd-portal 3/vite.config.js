9879146980f2e5ba6580d750ad2db01f3a8ba210
chikwavak-hub <chikwavak@gmail.com>
2026-05-07 14:08:09 -0400

    Update vite.config.js

--- bsbd-portal 3/vite.config.js
+++ bsbd-portal 3/vite.config.js
@@ -5,6 +5,12 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    sourcemap: true,
+    rollupOptions: {
+      external: [],
+    }
+  },
+  optimizeDeps: {
+    exclude: ['pdfjs-dist']
   }
 })
